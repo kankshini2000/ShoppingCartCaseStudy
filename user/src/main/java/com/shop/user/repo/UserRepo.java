d315facf818f6b9ca48fc705884fc99a7ed6ef60
kankshini2000 <84704527+kankshini2000@users.noreply.github.com>
2022-05-31 18:29:33 +0530

    Delete user directory

--- user/src/main/java/com/shop/user/repo/UserRepo.java
+++
@@ -1,9 +0,0 @@
-package com.shop.user.repo;
-
-import com.shop.user.Model.User;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface UserRepo extends MongoRepository<User,String>{
-    
-}
